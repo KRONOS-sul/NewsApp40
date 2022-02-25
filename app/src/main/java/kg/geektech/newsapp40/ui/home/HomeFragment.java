64f911c3581212c4bd84ac92deb027563667831f
KRONOS <vortex0075@mail.ru>
2022-02-25 16:24:38 +0300

    2

--- app/src/main/java/kg/geektech/newsapp40/ui/home/HomeFragment.java
+++ app/src/main/java/kg/geektech/newsapp40/ui/home/HomeFragment.java
@@ -15,16 +15,31 @@ import androidx.fragment.app.FragmentResultListener;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 import kg.geektech.newsapp40.R;
 import kg.geektech.newsapp40.databinding.FragmentHomeBinding;
 import kg.geektech.newsapp40.models.News;
+import kg.geektech.newsapp40.ui.news.NewsAdapter;
 
 public class HomeFragment extends Fragment {
 
+    private NewsAdapter adapter;
     private FragmentHomeBinding binding;
+    private Object NewsAdapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new NewsAdapter();
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,17 +51,24 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.recycler.setAdapter(adapter);
+
+        Date currentTime = Calendar.getInstance().getTime();// Здесь получаю время
+        String formattedDate = DateFormat.getDateInstance(DateFormat.FULL).format(currentTime);
+        Log.e("ggggg", "GG: " + formattedDate);
         
 
         binding.fab.setOnClickListener(view1 -> {
             openFragment();
         });
-        getParentFragmentManager().setFragmentResultListener("rk_news", getViewLifecycleOwner(), new FragmentResultListener() {
+        binding.recycler.setAdapter((RecyclerView.Adapter) NewsAdapter);
+        getParentFragmentManager().setFragmentResultListener("rk_news",
+                getViewLifecycleOwner(), new FragmentResultListener() {
             @Override
             public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
                 News news = (News) result.getSerializable("news");
+                adapter.addItem(news);
                 Log.e("ggggg", "GG: " + news.getTitle());
-
             }
         });
     }
--- app/src/main/java/kg/geektech/newsapp40/ui/news/NewsFragment.java
+++ app/src/main/java/kg/geektech/newsapp40/ui/news/NewsFragment.java
@@ -1,4 +1,4 @@
-package kg.geektech.newsapp40;
+package kg.geektech.newsapp40.ui.news;
 
 import android.os.Bundle;
 
@@ -7,11 +7,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import kg.geektech.newsapp40.R;
 import kg.geektech.newsapp40.databinding.FragmentNewsBinding;
 import kg.geektech.newsapp40.models.News;
 
